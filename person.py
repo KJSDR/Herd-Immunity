47d5e57cf3b4af507ce9492e46faa878ca847d55
KJSDR <killian.delamarre-ruer@students.dominican.edu>
2024-12-09 09:10:45 -0800

    More changes to classes

--- person.py
+++ person.py
@@ -1,79 +1,44 @@
 import random
 from virus import Virus
 
-class Person(object):
-    # Define a person.
-    def __init__(self, _id, is_vaccinated, infection=None):
-        # A person has an id, is_vaccinated, and possibly an infection
-        self._id = _id  # int
-        self.is_vaccinated = is_vaccinated  # Boolean value for vaccination status
-        self.infection = infection  # This will hold a Virus object or None
-        self.is_alive = True  # By default, the person is alive
+class Individual:
+    """Represents an individual in the population."""
 
-    def did_survive_infection(self):
-        # This method checks if a person survived an infection. 
+    def __init__(self, id, vaccinated, infection=None):
+        """Initializes an individual with ID, vaccination status, and optional infection."""
+        self.id = id
+        self.vaccinated = vaccinated
+        self.infection = infection
+        self.alive = True
+
+    def survive_or_die(self):
+        """Determines if the person survives the infection based on the mortality rate."""
         if self.infection:
-            # Generate a random number between 0.0 and 1.0
-            survival_chance = random.random()  # Random number between 0.0 and 1.0
-            # Check if the random number is less than the mortality rate of the virus
-            if survival_chance < self.infection.mortality_rate:
-                # Person dies, mark as not alive
-                self.is_alive = False
-                return False  # Person did not survive
+            survival_prob = random.random()
+            print(f"Person {self.id} | Infection: {self.infection.name} | Survival Chance: {survival_prob}, Mortality: {self.infection.mortality_rate}")
+            if survival_prob < self.infection.mortality_rate:
+                self.alive = False
+                self.infection = None
+                print(f"Person {self.id} succumbed to the infection.")
+                return False
             else:
-                # Person survives, they become vaccinated
-                self.is_vaccinated = True
-                return True  # Person survived
-        return True  # If no infection, the person is considered to have survived
+                self.vaccinated = True
+                self.infection = None
+                print(f"Person {self.id} survived and is now vaccinated.")
+                return True
+        return True
 
 if __name__ == "__main__":
-    # Test a vaccinated person
-    vaccinated_person = Person(1, True)
-    assert vaccinated_person._id == 1
-    assert vaccinated_person.is_alive is True
-    assert vaccinated_person.is_vaccinated is True
-    assert vaccinated_person.infection is None
+    # Example test cases for individuals
+    virus_instance = Virus("TestVirus", 0.5, 0.2)
 
-    # Create an unvaccinated person and test their attributes
-    unvaccinated_person = Person(2, False)
-    assert unvaccinated_person._id == 2
-    assert unvaccinated_person.is_alive is True
-    assert unvaccinated_person.is_vaccinated is False
-    assert unvaccinated_person.infection is None
+    # Create an unvaccinated person
+    person1 = Individual(1, False, virus_instance)
+    person2 = Individual(2, True)
 
-    # Test an infected person. An infected person has an infection/virus
-    virus = Virus("Dysentery", 0.7, 0.2)  # Virus with 70% mortality rate and 20% infection rate
-    infected_person = Person(3, False, virus)
-    assert infected_person._id == 3
-    assert infected_person.is_vaccinated is False
-    assert infected_person.infection is virus
-
-    # Test survival of an infected person
-    survived = infected_person.did_survive_infection()
-    if survived:
-        assert infected_person.is_vaccinated is True
-        assert infected_person.is_alive is True
-    else:
-        assert infected_person.is_alive is False
-
-    # Create 100 people and resolve survival of infection based on mortality rate
-    people = []
-    for i in range(1, 101):
-        person = Person(i, random.random() < 0.5)  # 50% chance of vaccination
-        if random.random() < 0.5:  # 50% chance of being infected
-            person.infection = virus
-        people.append(person)
-
-    # Track survival rates
-    did_survive = 0
-    did_not_survive = 0
-
-    for person in people:
-        survived = person.did_survive_infection()
-        if person.is_alive:
-            did_survive += 1
-        else:
-            did_not_survive += 1
-
-    print(f"Survived: {did_survive} people")
-    print(f"Did not survive: {did_not_survive} people")
+    # Simulate survival
+    result1 = person1.survive_or_die()
+    result2 = person2.survive_or_die()
+    
+    print(f"Person 1 survived: {result1}")
+    print(f"Person 2 survived: {result2}")
