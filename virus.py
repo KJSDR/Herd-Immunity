22e9e566a4bcde79bd80f55a339d01b4f3d8bd30
KJSDR <killian.delamarre-ruer@students.dominican.edu>
2024-12-13 13:36:45 -0800

    fixes

--- virus.py
+++ virus.py
@@ -1,27 +1,26 @@
-class Virus(object):
-    # Properties and attributes of the virus used in Simulation.
-    def __init__(self, name, repro_rate, mortality_rate):
+class Virus:
+    def __init__(self, name, reproduction_rate, mortality_rate):
+        """
+        Initializes a virus with specified attributes.
+        """
         self.name = name
-        self.repro_rate = repro_rate
+        self.reproduction_rate = reproduction_rate
         self.mortality_rate = mortality_rate
 
-def test_valid_virus_attributes():
-    virus = Virus("TestVirus", 0.5, 0.25)
-    assert virus.repro_rate == 0.5
-    assert virus.mortality_rate == 0.25
+def test_virus_attributes():
+    test_virus = Virus("TestVirus", 0.5, 0.25)
+    assert test_virus.reproduction_rate == 0.5
+    assert test_virus.mortality_rate == 0.25
 
-def test_invalid_virus_attributes():
+def test_invalid_virus():
     try:
         Virus("TestVirus", -0.1, 1.5)
     except ValueError:
         assert True
 
-
-
-# Test this class
 if __name__ == "__main__":
     virus = Virus("HIV", 0.8, 0.3)
     assert virus.name == "HIV"
-    assert virus.repro_rate == 0.8
+    assert virus.reproduction_rate == 0.8
     assert virus.mortality_rate == 0.3
     print("All tests passed!")
